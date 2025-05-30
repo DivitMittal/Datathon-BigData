91faf9fd8ac9f5337cd7c40223f1725aa5ab5a6c
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-31 02:50:45 +0530

    feat(nix): improve nix support & add nix direnv support
    -> add nix stuff to .gitignore
    -> add flake.lock
    -> .envrc

--- flake.nix
+++ flake.nix
@@ -1,14 +1,5 @@
 {
   description = "Datathon flake";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
 
   outputs = {flake-parts, ...} @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -35,4 +26,14 @@
         projectRootFile = "flake.nix";
       };
     };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
